0d28d1b888bd00bcf543c3e1dbc59e24048ce0f9
NzauM <mercywaenu16@gmail.com>
2019-11-08 16:42:50 +0300

    Add function to search a user

--- tests.py
+++ tests.py
@@ -13,6 +13,7 @@ class TestUsers(unittest.TestCase):
         '''
         self.new_user = Users("Merciee","Mercy","Nzau","October","Mercy10")
 
+
     def tearDown(self):
         '''
         The tear down method which will clean up after every test case is complete
@@ -22,6 +23,7 @@ class TestUsers(unittest.TestCase):
         '''
         This will remove every test user instance after evry test case is complete
         '''
+
 
     def test_init(self):
         '''
@@ -41,8 +43,7 @@ class TestUsers(unittest.TestCase):
         self.new_user.save_users()
         self.assertEqual(len(Users.users_list),1)
 
-
-    f test_save_moreThanOne_USer(self):
+    def test_save_moreThanOne_USer(self):
         '''
         Testcase to test whether more than one user can be added to the user list
         '''
@@ -52,8 +53,35 @@ class TestUsers(unittest.TestCase):
         test_user.save_users()
         self.assertEqual(len(Users.users_list),2)
 
+    def test_find_user_byPassword(self):
+        '''
+        To see if we can find he details of a user by using their username
+        Will help us to match users to their passwords
+        '''
+        self.new_user.save_users()
+        test_user = Users("BeckyJ","Becka","Mbulwa","July","Becka7")
+        test_user.save_users()
+
+        User_found = Users.find_user_byPassword("Becka7")
+        self.assertEqual(User_found.password,test_user.password)
+
+    def test_user_registered(self):
+        '''
+        To check if a user is already registered
+        '''
+        self.new_user.save_users()
+        test_user = Users("BeckyJ","Becka","Mbulwa","July","Becka7")
+        test_user.save_users()
+
+        user_registered = Users.user_registered("BeckyJ")
+        
+        self.assertTrue(user_registered)
+
     
+
+
      
+
 
 
 
